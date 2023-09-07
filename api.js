218567f882a3e4ac8ab2e3ad41c7c46d4e8f0ec2
leoemaxie <leoemaxie@gmail.com>
2023-09-07 01:58:09 +0100

    GET parameterized slack_name and track

--- api.js
+++ api.js
@@ -3,13 +3,14 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
+    const { slack_name, track } = req.query
     const response = {
-        slack_name: "Emmanuel Lafenwa",
+        slack_name,
         current_day: new Date().toLocaleDateString('en-US', {weekday: 'long'}),
         utc_time: new Date().toISOString(),
-        track: "backend",
+        track,
         github_repo_url: "https://github.com/leoemaxie/api-endpoint",
-        github_file_url: "https://github.com/leoemaxie/api-endpoint/blob/main/api.ext",
+        github_file_url: "https://github.com/leoemaxie/api-endpoint/blob/main/api.js",
         status_code: "200"
     }
     res.json(response);
